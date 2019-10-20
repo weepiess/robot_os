227456277542206a9959253fa436db7ab7489afc
weepiess <1095215611@qq.com>
2019-10-20 23:10:47 +0800

    add cam cap

--- robot_cam_uestc/nodes/mind_vision_cam_node.cpp
+++ robot_cam_uestc/nodes/mind_vision_cam_node.cpp
@@ -25,16 +25,48 @@
 #include "robot_cam_uestc/mind_vision_cam_dev.h"
 #include "robot_cam/robot_cam_example.h"
 #include "robot_tool/time_tool.h"
-
+#include <time.h>
 #include <signal.h>
 #include "ros/package.h"
 #include "opencv2/dnn.hpp"
 using namespace cv;
 using namespace std;
+using namespace dnn;
+
+vector<String> getOutputsNames(const Net& net)
+{
+	static vector<String> names;
+	if (names.empty())
+	{
+		//Get the indices of the output layers, i.e. the layers with unconnected outputs
+		vector<int> outLayers = net.getUnconnectedOutLayers();
+ 
+		//get the names of all the layers in the network
+		vector<String> layersNames = net.getLayerNames();
+ 
+		// Get the names of the output layers in names
+		names.resize(outLayers.size());
+		for (size_t i = 0; i < outLayers.size(); ++i)
+			names[i] = layersNames[outLayers[i] - 1];
+	}
+	return names;
+}
 
 
 int main(int argc, char **argv){
-    Net net;
+
+tm tm_; 
+time_t t_; 
+char buf[128]= {0}; 
+
+
+    // std::string modelConfiguration = ros::package::getPath("robot_cam_uestc") + "/res/yolov3-tiny.cfg";
+    // std::string modelWeights = ros::package::getPath("robot_cam_uestc") + "/res/yolov3-tiny_500.weights";
+    // Net net = readNetFromDarknet(modelConfiguration, modelWeights);
+    
+    // net.setPreferableBackend(3);
+    // net.setPreferableTarget(1);
+
     std::string config_path = ros::package::getPath("robot_cam_uestc") + "/res/mind_vision_cam_config.yaml";
     std::string save_pic_path = ros::package::getPath("robot_cam_uestc") + "/save_pic/";
     //根据敌方颜色初始化相机模块
@@ -47,18 +79,37 @@ int main(int argc, char **argv){
     // robot_cam::RobotCamExample cam_example;
     // cam_example.init(&cam_intercace, config_path);
     Mat src;
+    bool movie_cap = false;
+    // vector<Mat> outs;
     robot_tool::TimeTool timetool;
     int pic_count = 0;
+    VideoWriter writer;
+    writer.open(save_pic_path+"movie.avi",CV_FOURCC('M', 'J', 'P', 'G'), 10, Size(960,600), true);
     while(true){
+        time(&t_);
+        tm_ = *localtime(&t_);
+        strftime(buf, 64, "%Y-%m-%d %H:%M:%S", &tm_); 
+
         int start = timetool.currentTimeMsGet();
+
+        cout<<"t is:"<<buf+to_string(start)<<endl;
         cam_intercace.getImg(src);
+        // net.setInput(src);
+        // net.forward(outs, getOutputsNames(net));
         int end = timetool.currentTimeMsGet();
         cout<<"time cost is: "<<(end-start)<<" size: "<<src.size()<<"\n";
         imshow("Src",src);
+        if(movie_cap){
+            writer.write(src);
+        }
         char key=cv::waitKey(1);
         if(key=='o'){
-            imwrite(save_pic_path + to_string(pic_count)+".jpg",src);
+
+            imwrite(save_pic_path + buf+to_string(start)+".jpg",src);
             pic_count++;
+        }
+        if(key == 'p'){
+            movie_cap = true;
         }
         cam_intercace.adjustParams(key);
     }
@@ -66,3 +117,4 @@ int main(int argc, char **argv){
     //ros::waitForShutdown();
     return EXIT_SUCCESS;
 }
+
